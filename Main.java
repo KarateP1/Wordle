26493efd9724e527df5ff09d6393452bf6c98074
KarateP1 <Bannaflash08@gmail.com>
2025-04-24 11:25:15 -0500

    Kinda sorta done.

--- Main.java
+++ Main.java
@@ -2,6 +2,25 @@ import java.util.*;
 import java.io.*;
 
 Public static void main(String[]args){
+  Scanner x = new Scanner(System.in);
+Puzzle game = new Puzzle();
 
+  while(7==7){
+    System.out.println("Please enter your guess");
+    game.newGame();
+    String me = x.next();
+    game.Guess(me);
+    while(!me.equals(game.getWord())){
+      me = x.next();
+      game.Guess(me)
+    }
+    System.out.println("Congrats! You guessed the word!");
+    System.out.println("Would you like to play again? Y or N")
+    if(!x.next().toLowerCase().equals("y")){
+      break;
+    }
+    
+    
+  }
   
 }
